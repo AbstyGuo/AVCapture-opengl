542d0363f67d48acbddae9eeccf6439f67d45295
guoyf <guoyf@mapbar.com>
2018-03-13 14:04:20 +0800

    Initial Commit

---
+++ AVCapture+opengl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVCapture+opengl
+//
+//  Created by guoyf on 2018/3/13.
+//  Copyright © 2018年 guoyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVCapture+opengl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVCapture+opengl
+//
+//  Created by guoyf on 2018/3/13.
+//  Copyright © 2018年 guoyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
